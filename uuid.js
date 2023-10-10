5b343f20d5d4a9611d242298fc63a6556a254935
huangguiguang <1614981899@qq.com>
2023-10-10 14:53:31 +0800

    study: 记录一道宏微任务题目

--- uuid.js
+++ uuid.js
@@ -1,2 +1,47 @@
 const uuid = require('uuid')
 console.log(uuid.v4())
+
+async function async1() {
+    console.log('1')
+    await async2()
+    console.log('aaa')
+    // await new Promise((resolve) => {
+    //     console.log('3')
+    //     resolve()
+    //     return new Promise((resolve) => {
+    //         resolve()
+    //         console.log('4')
+    //     })
+    // }).then(() => {
+    //     console.log('aaa')
+    // })
+}
+
+async function async2() {
+    console.log('3')
+    return new Promise((resolve) => {
+        resolve()
+        console.log('4')
+    })
+}
+
+console.log('5')
+
+setTimeout(() => {
+    console.log('6')
+}, 0)
+
+async1()
+
+new Promise((resolve) =>{
+    console.log('7')
+    resolve()
+}).then(() => {
+    console.log('8')
+}).then(() => {
+    console.log('9')
+}).then(() => {
+    console.log('10')
+})
+
+console.log('11')
